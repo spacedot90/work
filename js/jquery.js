8ed4f249532044a229d09fb04edb91048767e180
spacedot90 <104989473+spacedot90@users.noreply.github.com>
2022-05-05 21:49:33 +0900

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+// // Jquery
+//   $(document).ready(function(){
+//       $(".title_des").click(function(){
+//       $(this).hide();
+//       });
+//   });
+
+//   function popup(){
+//       console.log('aaaaaa')
+//   }
